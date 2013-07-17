cdfcde9ba786cfcff74c71c1cc1cde48fe5b8345
Eric Heininger <ericheininger@gmail.com>
2013-07-17 18:58:29 +0000

    hopefully the last web.js push

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('hello world2!');
+var fs = require('fs');
+var buffer = new buffer(fs.readFileSyn('index.html'), 'utf-8');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
