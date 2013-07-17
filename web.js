65f01bcad9a85f3a38b4acaa751885a9d8b879af
Ubuntu <ubuntu@ip-172-31-27-203.us-west-2.compute.internal>
2013-07-17 21:23:48 +0000

    Added both from new instance

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var buffer = new Buffer(fs.readFileSync('index.html'), 'utf-8');
 response.send(fs.readFileSync('index.html').toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
